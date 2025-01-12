92dfe4cdda66b7f5b474ba4fc6bc2d3efe342df5
henntaidesu <498791444@qq.com>
2025-01-12 16:40:14 +0900

    新增内部功能

--- src/asmr_api/get_down_list.py
+++ src/asmr_api/get_down_list.py
@@ -3,8 +3,13 @@ from src.read_conf import ReadConf
 
 
 def get_down_list():
-    url = f'https://api.asmr-200.com/api/review?order=updated_at&sort=desc&page=1&filter=marked'
     conf = ReadConf()
+    check_DB = conf.check_DB()
+    if check_DB:
+        url = f'https://api.asmr-200.com/api/review?order=updated_at&sort=desc&page=1&filter=listening'
+    else:
+        url = f'https://api.asmr-200.com/api/review?order=updated_at&sort=desc&page=1&filter=marked'
+
     user_data = conf.read_asmr_user()
     token = user_data['token']
     headers = {
---
+++ src/datebase_execution.py
@@ -0,0 +1,104 @@
+import sys
+from src.read_conf import ReadConf
+import sqlite3
+
+
+def TrimString(Str):
+    # if '\n' in Str:
+    #     Str = Str.replace('\n', ' ')
+    # if ' ' in Str:
+    #     Str = Str.replace(' ', '')
+    # if '/' in Str:
+    #     Str = Str.replace('/', ' ')
+    if "'" in Str:
+        Str = Str.replace("'", "\\'")
+    if '"' in Str:
+        Str = Str.replace('"', '\\"')
+    return Str
+
+
+class MySQLDB:
+    def __init__(self):
+        read_db_conf = ReadConf()
+        self.db = read_db_conf.read_database()
+
+    def insert(self, sql):
+        cursor = self.db.cursor()
+        cursor.execute(sql)
+        self.db.commit()
+        self.db.close()
+        return True
+
+    def update(self, sql):
+        cursor = self.db.cursor()
+        cursor.execute(sql)
+        self.db.commit()
+        cursor.close()
+        return True
+
+
+    def select(self, sql):
+        cursor = self.db.cursor()
+        cursor.execute(sql)
+        result = cursor.fetchall()
+        cursor.close()
+        return True, result
+
+
+    def delete(self, sql):
+        cursor = self.db.cursor()
+        cursor.execute(sql)
+        self.db.commit()
+        cursor.close()
+
+
+
+class SQLiteDB:
+    def __init__(self):
+        self.db = None
+        self.db_path = "db.db"
+        self.connect_db()
+
+    def connect_db(self):
+        """连接SQLite数据库"""
+        try:
+            self.db = sqlite3.connect(self.db_path)
+        except sqlite3.Error as e:
+            print(f"数据库连接失败: {str(e)}", 'error')
+            sys.exit()
+
+    def execute_query(self, sql, query_type='select'):
+        """
+        执行SQL查询，根据类型返回不同的结果。
+        query_type: select, insert, update, delete
+        """
+        try:
+            cursor = self.db.cursor()
+            cursor.execute(sql)
+
+            if query_type == 'select':
+                result = cursor.fetchall()
+                return True, result
+            elif query_type in ['insert', 'update', 'delete']:
+                self.db.commit()  # 对于修改操作，提交更改
+                return True
+        except sqlite3.Error as e:
+            print(f"数据库操作失败: {str(e)}", 'error')
+            return False
+
+
+    def insert(self, sql):
+        return self.execute_query(sql, 'insert')
+
+    def update(self, sql):
+        return self.execute_query(sql, 'update')
+
+    def select(self, sql):
+        return self.execute_query(sql, 'select')
+
+    def delete(self, sql):
+        return self.execute_query(sql, 'delete')
+
+    def close_connection(self):
+        if hasattr(self, 'db') and self.db:
+            self.db.close()
