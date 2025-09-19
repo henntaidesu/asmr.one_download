557370ccb48f3787e372572a3cbf4e1b28aace7b
henntaidesu <498791444@qq.com>
2025-09-19 14:26:27 +0900

    修复 登录获取Token时候程序实例化SQLiteDB实例导致的闪退问题

--- src/datebase_execution.py
+++ src/datebase_execution.py
@@ -53,52 +53,53 @@ class MySQLDB:
 
 
 
-class SQLiteDB:
-    def __init__(self):
-        self.db = None
-        self.db_path = "db.db"
-        self.connect_db()
+# class SQLiteDB:
+#     def __init__(self):
+#         self.db = None
+#         self.db_path = "db.db"
+#         self.connect_db()
 
-    def connect_db(self):
-        """连接SQLite数据库"""
-        try:
-            self.db = sqlite3.connect(self.db_path)
-        except sqlite3.Error as e:
-            print(f"数据库连接失败: {str(e)}", 'error')
-            sys.exit()
+#     def connect_db(self):
+#         """连接SQLite数据库"""
+#         try:
+#             self.db = sqlite3.connect(self.db_path)
+#         except sqlite3.Error as e:
+#             print(f"数据库连接失败: {str(e)}")
+#             self.db = None
+#             raise sqlite3.Error(f"数据库连接失败: {str(e)}")
 
-    def execute_query(self, sql, query_type='select'):
-        """
-        执行SQL查询，根据类型返回不同的结果。
-        query_type: select, insert, update, delete
-        """
-        try:
-            cursor = self.db.cursor()
-            cursor.execute(sql)
+#     def execute_query(self, sql, query_type='select'):
+#         """
+#         执行SQL查询，根据类型返回不同的结果。
+#         query_type: select, insert, update, delete
+#         """
+#         try:
+#             cursor = self.db.cursor()
+#             cursor.execute(sql)
 
-            if query_type == 'select':
-                result = cursor.fetchall()
-                return True, result
-            elif query_type in ['insert', 'update', 'delete']:
-                self.db.commit()  # 对于修改操作，提交更改
-                return True
-        except sqlite3.Error as e:
-            print(f"数据库操作失败: {str(e)}", 'error')
-            return False
+#             if query_type == 'select':
+#                 result = cursor.fetchall()
+#                 return True, result
+#             elif query_type in ['insert', 'update', 'delete']:
+#                 self.db.commit()  # 对于修改操作，提交更改
+#                 return True
+#         except sqlite3.Error as e:
+#             print(f"数据库操作失败: {str(e)}", 'error')
+#             return False
 
 
-    def insert(self, sql):
-        return self.execute_query(sql, 'insert')
+#     def insert(self, sql):
+#         return self.execute_query(sql, 'insert')
 
-    def update(self, sql):
-        return self.execute_query(sql, 'update')
+#     def update(self, sql):
+#         return self.execute_query(sql, 'update')
 
-    def select(self, sql):
-        return self.execute_query(sql, 'select')
+#     def select(self, sql):
+#         return self.execute_query(sql, 'select')
 
-    def delete(self, sql):
-        return self.execute_query(sql, 'delete')
+#     def delete(self, sql):
+#         return self.execute_query(sql, 'delete')
 
-    def close_connection(self):
-        if hasattr(self, 'db') and self.db:
-            self.db.close()
+#     def close_connection(self):
+#         if hasattr(self, 'db') and self.db:
+#             self.db.close()
